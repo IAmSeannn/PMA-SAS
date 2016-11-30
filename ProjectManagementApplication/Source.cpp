bd6cd302a0b70883a554369d4830c5582a04bf23
IAmSeannn <iamseannn@gmail.com>
2016-11-30 20:00:15 +0000

    Added example code to test tinyxml2 features
    example xml from w3schools website, and example code from the tinyxml2
    readme.

--- ProjectManagementApplication/Source.cpp
+++ ProjectManagementApplication/Source.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include "tinyxml2.h"
 
 int main(int argc, char* argv[])
 {
+
+	tinyxml2::XMLDocument doc;
+	doc.LoadFile("../ProjectManagementApplication/testing.xml");
+
+	const char* title = doc.FirstChildElement("note")->FirstChildElement("body")->GetText();
+	printf("Name of play (1): %s\n", title);
+
+	tinyxml2::XMLText* textNode = doc.FirstChildElement("note")->FirstChildElement("to")->FirstChild()->ToText();
+	title = textNode->Value();
+	printf("addressed to (2): %s\n", title);
 
 	std::cin.get();
 	std::cin.get();
