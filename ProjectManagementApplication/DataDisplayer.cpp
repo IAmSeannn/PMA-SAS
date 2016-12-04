cb903c59bc4e673c7d9a230cb6bbe83722d39371
IAmSeannn <iamseannn@gmail.com>
2016-12-04 19:04:27 +0000

    Program Now Prints Nicely
    Nicely being a subjective term. Please i am not an artist.

--- ProjectManagementApplication/DataDisplayer.cpp
+++ ProjectManagementApplication/DataDisplayer.cpp
@@ -10,3 +10,42 @@ DataDisplayer::DataDisplayer()
 DataDisplayer::~DataDisplayer()
 {
 }
+
+void DataDisplayer::PrintTest(DataCzar data)
+{
+	PrintPageLine();
+	std::cout << "---Excellent Project Manager v1.0---\n";
+	PrintPageLine();
+	std::cout << "Number of projects loaded: " << data.GetProjects().size() << "\n";
+	PrintPageLine();
+	std::cout << "Currently Loaded Projects:\n";
+	PrintSubLine();
+	for (Project p : data.GetProjects())
+	{
+		std::cout << "	¬Project Name: " << p.GetName() << "\n";
+		std::cout << "	¬Tasks Involved:\n";
+		PrintSubLine();
+		for (Task t : p.GetTasks())
+		{
+
+			std::cout << "		¬Task Name: " << t.GetName() << "\n";
+			std::cout << "		¬Time Spent On Task:\n";
+			PrintSubLine();
+			for (TimeAllocation* pT : t.GetTAs())
+			{
+				std::cout << "			¬" << pT->GetDetails() << "\n";
+			}
+		}
+		PrintPageLine();
+	}
+}
+
+void DataDisplayer::PrintPageLine()
+{
+	std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+}
+
+void DataDisplayer::PrintSubLine()
+{
+	std::cout << "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n";
+}
--- ProjectManagementApplication/DataDisplayer.h
+++ ProjectManagementApplication/DataDisplayer.h
@@ -7,29 +7,10 @@ class DataDisplayer
 public:
 	DataDisplayer();
 	~DataDisplayer();
-	static void PrintTest(DataCzar data)
-	{
-		std::cout << "Number of projects is :: " << data.GetProjects().size() << "\n";
+	static void PrintTest(DataCzar data);
 
-		std::cout << "Here is a list of prohects, by name :: \n\n\n";
-
-		for (Project p : data.GetProjects())
-		{
-			std::cout << p.GetName() << "\n";
-			std::cout << "Tasks Involved:: \n";
-
-			for (Task t : p.GetTasks())
-			{
-				std::cout << t.GetName() << "\n";
-
-				for (TimeAllocation* pT : t.GetTAs())
-				{
-					std::cout << "          -" << pT->GetDetails() << "\n";
-				}
-			}
-
-			std::cout << "\n";
-		}
-	}
+private:
+	static void PrintPageLine();
+	static void PrintSubLine();
 };
 
