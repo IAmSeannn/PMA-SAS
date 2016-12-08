1f045ce9d45b2fa77e479460f733b94a6edad1b9
IAmSeannn <iamseannn@gmail.com>
2016-12-08 18:36:03 +0000

    Added more sort options
    User can now choose whether to display all the information or just TAs
    before choosing how to sort.

--- ProjectManagementApplication/Source.cpp
+++ ProjectManagementApplication/Source.cpp
@@ -8,12 +8,45 @@
 void CommandLoadXMLFIle();
 void CommandLoadMainMenu();
 void CommandLoadDisplayData();
+void CommandDisplayTAsOnly();
 void CommandSortData();
 void CommandSortTAs(bool order); //0 for assending, 1 for desending
 
 void CommandSortData()
 {
 	RuntimeMenu::DisplayTitle();
+
+	std::cout << "Would you like to see the Time Allocations grouped with appropriate tasks and projects? Y/N\n";
+	bool success = false;
+	char response;
+	while (!success)
+	{
+		std::cin >> response;
+
+		switch (response)
+		{
+		case 'y':
+			//CommandLoadDisplayData();
+			CommandSortTAs(true);
+			break;
+		case 'n':
+			CommandSortTAs(false);
+			//CommandDisplayTAsOnly();
+			break;
+		default:
+			std::cout << "That command is no recognised, please try again.\n";
+			break;
+		}
+	}
+}
+
+const bool IsGreaterThan(TimeAllocation* lhs, TimeAllocation* rhs)
+{
+	return (*lhs) < (*rhs);
+}
+
+void CommandSortTAs(bool full) //true for assending, false for desending
+{
 	std::cout << "Before displaying the data, how would you like it sorted?\n";
 	std::cout << "1. Assending\n";
 	std::cout << "2. Desending\n";
@@ -29,42 +62,71 @@ void CommandSortData()
 		case 1:
 			responseSuccess = true;
 			//assending
-			CommandSortTAs(true);
 			break;
 		case 2:
 			responseSuccess = true;
 			//desending
-			CommandSortTAs(false);
 			break;
 		default:
 			std::cout << "Command not recognised, please try again.\n";
 			break;
 		}
 	}
-}
 
-const bool IsGreaterThan(TimeAllocation* lhs, TimeAllocation* rhs)
-{
-	return (*lhs) < (*rhs);
-}
-
-void CommandSortTAs(bool order) //true for assending, false for desending
-{
-	for (Project &p : DataCzar::Current->GetProjects())
+	if (full)
 	{
-		for (Task &t : p.GetTasks())
+		for (Project &p : DataCzar::Current->GetProjects())
 		{
-			std::sort(t.GetTAs().begin(), t.GetTAs().end(), IsGreaterThan);
-
-			if (order)
+			for (Task &t : p.GetTasks())
 			{
-				//reverse the vector
-				std::reverse(t.GetTAs().begin(), t.GetTAs().end());
+				std::sort(t.GetTAs().begin(), t.GetTAs().end(), IsGreaterThan);
+
+				if (response == 1)
+				{
+					//reverse the vector
+					std::reverse(t.GetTAs().begin(), t.GetTAs().end());
+				}
 			}
 		}
+
+		CommandLoadDisplayData();
+	}
+	else
+	{
+		std::vector<TimeAllocation*> master;
+		//add all TAs in entire dataczar
+		for (Project &p : DataCzar::Current->GetProjects())
+		{
+			for (Task &t : p.GetTasks())
+			{
+				master.insert(master.end(), t.GetTAs().begin(), t.GetTAs().end());
+			}
+		}
+
+		std::sort(master.begin(), master.end(), IsGreaterThan);
+
+		if (response == 1)
+		{
+			//reverse the vector
+			std::reverse(master.begin(), master.end());
+		}
+
+		//then print them all
+		for (TimeAllocation* ta : master)
+		{
+			std::cout << ta << "\n";
+		}
+
+		std::cin.get();
+		std::cin.get();
 	}
 
-	CommandLoadDisplayData();
+	
+}
+
+void CommandDisplayTAsOnly()
+{
+	
 }
 
 void CommandLoadXMLFIle()
