fad0269192295d7f46fbc2dcda2f51426b4b7b32
IAmSeannn <iamseannn@gmail.com>
2016-12-11 22:17:30 +0000

    Require Data
    The program now requires data to be loaded before most of the
    functionaliy becomes available.

--- PMA-Redux/Source.cpp
+++ PMA-Redux/Source.cpp
@@ -1,5 +1,32 @@
 #include "DataCzar.h"
 #include "MenuSystem.h"
+#include "Utils.h"
+
+void LoadInitialFile()
+{
+	RuntimeMenu::DisplayTitle();
+	std::cout << "No arguments provided, or the data was not loaded sucessfully.\n";
+	std::cout << "Please input the path of a valid XML file.\n";
+
+	std::string inputPath;
+	bool success = false;
+	while (!success)
+	{
+		std::cin >> inputPath;
+		tinyxml2::XMLError error = DataCzar::Current->SetUp(inputPath);
+		if (error == tinyxml2::XML_SUCCESS)
+		{
+			std::cout << "Data loaded successfully. Press any key to continue...";
+			success = true;
+		}
+		else
+		{
+			std::cout << "ERROR: " << error << "\n";
+			std::cout << "Loading failed. The file may not exist or be mispelled. Please try again:\n";
+		}
+	}
+	Utils::Pause();
+}
 
 int main(int argc, char* argv[])
 {
@@ -12,6 +39,27 @@ int main(int argc, char* argv[])
 	//initialise static dataczar
 	std::shared_ptr<DataCzar> d(new DataCzar);
 	DataCzar::Current = d;
+
+	//check for args and try load
+	if (argc > 1)
+	{
+		tinyxml2::XMLError error = DataCzar::Current->SetUp(argv[1]);
+		if (error == tinyxml2::XML_SUCCESS)
+		{
+			RuntimeMenu::DisplayLoadXMLMenu();
+			std::cout << "Data loaded successfully. Press any key to continue...";
+			Utils::Pause();
+		}
+		else
+		{
+			LoadInitialFile();
+		}
+	}
+	else
+	{
+		LoadInitialFile();
+	}
+	
 
 	MenuSystem::CommandLoadMainMenu();
 }
